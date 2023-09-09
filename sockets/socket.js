6875b9064d521ee7bc3298e3424826b9fe26f63b
pancho <pancho@pancho.com>
2023-09-08 22:26:18 -0300

    llisto

--- sockets/socket.js
+++ sockets/socket.js
@@ -2,14 +2,14 @@
 const {io} = require('../index.js');
 const ListaBandas = require('../models/bandas.js');
 const Banda = require('../models/model_bandas.js');
-var puerto2=0;
+
 var listaBandas = new ListaBandas();
       listaBandas.agregarBanda(new Banda('Pancho'));
       listaBandas.agregarBanda(new Banda('Caro'));
       listaBandas.agregarBanda(new Banda('Estaban'));
       listaBandas.agregarBanda(new Banda('Larita'));
+     
 
-var datapuerto=process.env.PORT;
 
  
 
@@ -18,34 +18,26 @@ io.on('connection', client => {
     console.log('cliente conectado info socket');
     client.on('disconnect', () => { console.log('desconectado...socket ')});
   
-
-      client.on('esp32',(payload)=>{console.log("escucho de esp32")});
-          
-      client.on('estado-puerto',(payload)=>{
-        console.log('estado del puerto 2 ',payload);
-        client.emit('estado-puerto',(payload)=>{
-           if(payload==0){puerto2=1}
-           else{puerto2=0}; 
-        });
-      });
   
-      client.on('mensajex', (payload) => { 
+  
+    client.on('mensajex', (payload) => { 
       console.log('escucho data en server',payload);
       client.broadcast.emit('mensajex',payload); 
-
       
+    
+    
     });
-    client.emit('puerto',puerto2);
  
       client.emit('mensajex','datos de server');
-              
-      client.emit('datos',listaBandas);
-      client.emit('puerto',datapuerto);
       
+      
+    
+      
+      client.emit('datos',listaBandas);
       client.on('votacion',function(payload){
+
         console.log(payload['id']);
-        //listaBandas.votar(payload['id']);
-        listaBandas.votar(payload[0]);
+        listaBandas.votar(payload['id']);
         io.emit('datos',listaBandas);
       });
       
